5d8425365afd32bfaaa9c1f1d677c22186106026
collol <sos142004@gmail.com>
2025-09-20 18:35:52 +0700

    update Day09

---
+++ nvh-lession09-app/src/pages/NvhHome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NvhHome() {
+  return (
+    <div className='alert alert-success'>
+      <h1 className='text-center'>Welcome to, Huỳnh Nguyễn</h1>
+    </div>
+  )
+}
